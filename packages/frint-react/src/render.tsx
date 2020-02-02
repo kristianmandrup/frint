f88050a01d98bf91f63c7bacd10fdbc824c40e5a
Kristian Mandrup <kmandrup@gmail.com>
2020-02-02 19:21:33 +0000

    typescript :)

--- packages/frint-react/src/render.tsx
+++ packages/frint-react/src/render.tsx
@@ -1,11 +1,10 @@
 /* eslint-disable import/no-extraneous-dependencies, global-require */
-import React from 'react';
+import React from "react";
+import ReactDOM from "react-dom";
 
-import getMountableComponent from './components/getMountableComponent';
+import getMountableComponent from "./components/getMountableComponent";
 
 export default function render(app, node) {
   const MountableComponent = getMountableComponent(app);
-  const ReactDOM = require('react-dom');
-
   return ReactDOM.render(<MountableComponent />, node);
 }
